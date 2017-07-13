b741a2b63e06f41937d5bd4c681babdb9e5e9f06
Kajsa Unge <hej@kajsaunge.se>
2017-07-13 02:26:26 +0200

    Adds cups to dl calculations. Adds svg and animation

--- script/main.js
+++ script/main.js
@@ -1,12 +1,27 @@
 var constant = 32;
 var celsiusTemp = "";
+var deciliterAmount = "";
+
+
+function clearBox(elementClass)
+{
+    document.getElementsByClassName(elementClass).innerHTML = "";
+}
 
 function fahrenheitToCelsius(temperature) {
     var celsiusTemp = (temperature - constant) / 1.8;
     return Math.round(celsiusTemp) + "°C";
 }
+function cupsToDeciliter(cups) {
+    var deciliterAmount = cups * 2.365882365;
+    return Math.round(deciliterAmount) + "dl";
+}   
+
 function resetform() {
-document.getElementById("fahrenheitForm").reset();
+    document.getElementById("fahrenheitForm").reset();
+}
+function resetCupsForm() {
+    document.getElementById("cupsForm").reset();
 }
 
 
@@ -18,5 +33,23 @@ function showResult() {
     innerDiv.className = 'answer-container';
     document.getElementById("outputTemperature").appendChild(innerDiv).innerHTML = celsiusTemp; // 3
     resetform()
- 
 }
+
+
+
+function showDeciliterCalculation() {
+    // innerDiv.remove();
+    // document.getElementById("outputAmountDl").remove();
+    
+    var cups = document.getElementById("cups").value;
+    var deciliterAmount = cupsToDeciliter(cups);
+    
+    var innerDiv = document.createElement('h2');
+
+    innerDiv.className = 'answer-container';
+    var hej = document.getElementById("outputAmountDl").appendChild(innerDiv).innerHTML = deciliterAmount; // 3
+    resetCupsForm();   
+    clearBox('answer-container'); 
+}
+
+
